89a87f403a5e020a580a3889454e649ea3c2e74c
thanu-pspk <thanusha8610@gmail.com>
2026-01-19 09:59:31 +0530

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,131 @@
+var name = "thanusha"
+//let rollno = "59"
+//console.log( typeof rollno)
+//let changeOf = Number(rollno)
+//console.log(typeof changeOf)
+
+
+//let rollno = true ;
+//console.log( typeof rollno)
+//let changeOf = Number(rollno)
+//console.log(typeof changeOf)
+
+//let rollno = null
+//console.log( typeof rollno)
+//let changeOf = Number(rollno)
+//console.log(typeof changeOf)
+
+//let rollno = undefined
+//console.log( typeof rollno)
+//let changeOf = Number(rollno)
+//console.log(typeof changeOf)
+
+// undefined is not a number (NAN)
+
+//let rol = "null"
+//console.log( typeof rol)
+//let changOf = Null(rol)
+//console.log(typeof changOf)
+
+/*console.log(1 + "t")
+console.log("t" + 1)
+console.log(1 + 2 + "t")
+console.log("t" + 1 + 2)*/
+
+let p = 20;
+//p += 10;
+//p -= 10;
+//p *= 10;
+//p /= 10;
+//p %= 10;
+//console.log(p);
+
+/*if(null > 0 ){
+    console.log("true");
+
+}
+else{
+    console.log("false");
+    
+}*/
+/*console.log(null == 0);
+console.log(null < 0);
+console.log(null > 0);
+console.log(null <= 0);
+console.log(null >=0);*/
+
+// class is known as blueprint and the output is known as object 
+
+let name1 = "thanusha"
+/*console.log(name1.toUpperCase);
+console.log(name1.substring(0,5));
+console.log(name1.charAt(2));
+console.log(name1.indexOf('a'));
+console.log(name1.slice(0,6));
+console.log(name1.slice(0,-3));
+console.log(name1.trimEnd());*/
+/*console.log(`this is ${name1}`);
+let a = 10
+const b = a++
+console.log(`a:${a} and b: ${b}`);
+
+let x = 20
+const y = ++x
+console.log(`x:${x} and y:${y}`);*/
+
+
+/*let age = 12.123456
+console.log(age.toFixed(3));
+console.log(age.toExponential(2));
+console.log(age.toPrecision(2));
+console.log(age.valueOf());*/
+
+
+/*console.log(Math.floor(2.9));
+console.log(Math.ceil(2.1));
+console.log(Math.round(2.5));
+console.log(Math.max(2,55,65,88));
+console.log(Math.min(2,44,53,22,9));
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10)+1);*/
+
+/*let myDate = new Date();
+console.log(myDate.getdate());*/
+
+/*let arr = [1,3,4,5,2,99,"tahnusha",true]
+console.log(arr);
+arr.push(35)
+console.log(arr);
+arr.pop()
+console.log(arr);
+arr.unshift(135)
+console.log(arr);
+arr.shift()
+console.log(arr);
+
+console.log(arr.slice(1,4));
+console.log(arr);
+
+console.log(arr.splice(1,4));
+console.log(arr);*/
+
+
+let arr =[1,2,3,[44,55,88,],45,5,[11,77,75,[52,95,66],559],6]
+console.log(arr);
+console.log(arr.flat(0));
+console.log(arr.flat(1));
+console.log(arr.flat(2));
+
+let ar1 = [1,2,3,4,5]
+let ar2 =["thanu","pspk"]
+let ar3 = ar1. concat(ar2)
+console.log(ar3);
+
+
+
+
+
+
+
+
